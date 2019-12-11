f7c8c30715dd086f7bb8079ef1fc0061000167e2
Sachin Gurung <saching061@hotmail.com>
2019-12-10 21:53:00 -0800

    Add project files.

---
+++ Height categorize Tall, Dwarf or Average/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Height_categorize_Tall__Dwarf_or_Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
